57111a0f09a9acc43ea7c395b22c9b7e6b863e1d
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-11-22 17:21:10 -0500

    Added docstrings

--- gedcom.js
+++ gedcom.js
@@ -4,11 +4,16 @@
     
     var gedcom = {};
 
+    /**
+     * Implementation.
+     * @private
+     */
     function _parse(content) {
         var lines = content.trim().split(/\r?\n/);
         var records = [];
         var current = null;
         var sub, chi;
+        var ignored = [];
 
         // matches links to other records
         var re_link = new RegExp('^@([^@].*)@$');
@@ -67,17 +72,91 @@
             } else if (level === 0 && tag === 'TRLR') {
                 return; // end of file record
             } else {
+                ignored.push({
+                    line: index,
+                    content: line,
+                    record: current
+                });
                 console.log('Unhandled structure at line ' + index);
             }
             
         });
 
-        return records;
+        return {
+            records: records,
+            ignored: ignored
+        };
     }
 
+    /**
+     * Parses a gedcom file and constructs a raw JSON compatible object
+     * containing all information from the file.  Not all properties
+     * are handled by this parser.  Unhandled lines are written to
+     * the console and reported in the returned object.
+     *
+     * @param {(File|string)} file The file object or string content of a gedcom file.
+     * @param {function} done A callback function taking the parsed result as an argument.
+     *
+     * @example
+     * gedcom.parse(file, function (records, ignored) {
+     *   // records:
+     *   [
+     *     {
+     *       type: 'INDI',
+     *       id: 'I1'
+     *       NAME: [
+     *         value: 'John /Doe/',
+     *         'GIVN': [
+     *           'John'
+     *         ],
+     *         'SURN': [
+     *           'Doe'
+     *         ]
+     *       ],
+     *       ...
+     *     },
+     *     ...
+     *     {
+     *       type: 'FAM',
+     *       id: 'F1',
+     *       HUSB: [
+     *         {
+     *           value: 'I1'
+     *         }
+     *       ],
+     *       WIFE: [
+     *         {
+     *           value: 'I2'
+     *         }
+     *       ],
+     *       CHIL: [
+     *         ...
+     *       ]
+     *     },
+     *     ...
+     *   ]
+     *
+     *   // ignored: array of lines ignored
+     *   [
+     *     {
+     *       line: 100,
+     *       content: '3 SOME unhandled content',
+     *       record: {}  // The record containing the line.
+     *     }
+     *   ]
+     * });
+     */
     gedcom.parse = function (file, done) {
+        var result;
         if (typeof file === 'string') {
-            done(_parse(file));
+            result = _parse(file);
+            done(result.records, result.ignored);
+            return result.records;
+        } else {
+            // check for browswer compatibility
+            if (!GLOBAL.FileReader) {
+                throw new Error('Brower does not support HTTP5 FileReader.');
+            }
         }
         var reader = new FileReader();
 
@@ -96,8 +175,5 @@
         reader.readAsText(file);
     };
 
-    // check for browswer compatibility
-    if (GLOBAL.File && GLOBAL.FileReader && GLOBAL.FileList && GLOBAL.Blob) {
-        GLOBAL.gedcom = gedcom;
-    }
+    GLOBAL.gedcom = gedcom;
 })(window);
