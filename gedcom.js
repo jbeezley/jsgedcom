2a2008ce04f6ec56d25abdc0cf5f89744a3efaaf
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-11-23 15:24:53 -0500

    Implemented data normalization methods

--- gedcom.js
+++ gedcom.js
@@ -1,7 +1,9 @@
 /* jshint: browser */
 (function (GLOBAL) {
     'use strict';
-    
+    /**
+     * @namespace
+     */
     var gedcom = {};
 
     /**
@@ -149,7 +151,12 @@
     gedcom.parse = function (file, done) {
         var result;
         if (typeof file === 'string') {
-            result = _parse(file);
+            try {
+                result = _parse(file);
+            } catch(err) {
+                done(null, err);
+                return null;
+            }
             done(result.records, result.ignored);
             return result.records;
         } else {
@@ -161,7 +168,12 @@
         var reader = new FileReader();
 
         reader.onload = function (evt) {
-            done(_parse(evt.target.result));
+            try {
+                result = _parse(evt.target.result);
+            } catch(err) {
+                done(null, err);
+            }
+            done(result.records, result.ignored);
         };
 
         reader.onerror = function () {
@@ -174,6 +186,238 @@
 
         reader.readAsText(file);
     };
+
+    // caches data from geonames
+    var geocache = {};
+
+    /**
+     * Uses geonames service to get the coordinates of a place
+     * name.  The results are cached internally to limit API calls
+     * to geonames.
+     *
+     * @todo Unimplmented
+     */
+    gedcom.georeference = function (s, done) {
+        if (!geocache.hasOwnProperty(s)) {
+            geocache[s] = {
+                name: s,
+                longitude: 0,
+                latitude: 0
+            };
+        }
+        done(geocache[s]);
+    };
+
+    /**
+     * Generate a tree structure from the family records.
+     * For each person in the database, a new object is created
+     * with the following properties: ::
+     *   {
+     *     mothers: [],
+     *     fathers: [],
+     *     children: [],
+     *     spouses: [],
+     *     siblings: []
+     *   }
+     *
+     * @param {TAFFY} records A taffydb database of records from gedcom.parse
+     * @returns {object[]}
+     *
+     * @todo Unimplemented
+     */
+    gedcom.tree = function (records) {
+    };
+
+    /**
+     * Private normalization handlers.
+     * @private
+     */
+    function normalizeName(person) {
+        var names = person.NAME;
+        person.name = null;
+        delete person.NAME;
+
+        if (!names || !names.length) {
+            return 'No names present';
+        }
+
+        // maybe be smarter later, but for now just use the first
+        // object and throw away the rest.
+        name = names[0];
+
+        names.forEach(function (name) {
+            if (person.name) {
+                // already found
+                return;
+            }
+            var obj = {}, arr;
+            obj.first = 'UNKNOWN'; // allow unknown first name
+            obj.middle = []; // some people don't have exactly 1 middle name
+            if (name.SURN && name.SURN[0]) {
+                obj.last = name.SURN[0];
+            } else {
+                obj.last = name.value.match(/\/(.*)\//);
+                if (obj.last) {
+                    obj.last = obj.last[1];
+                }
+            }
+            if (name.GIVN && name.GIVN[0]) {
+                arr = name.GIVN[0].split(/\s+/);
+                if (arr[0]) {
+                    obj.first = arr[0];
+                }
+                obj.middle = arr.slice(1).join(' ');
+            }
+            person.name = obj;
+        });
+    }
+
+    function normalizeDate(d) {
+        // The standard date method seems to do a pretty good job with this.
+        // Maybe handle invalid values here?
+        return new Date(d);
+    }
+
+    // try to get the best date out of an array of date strings
+    // for now just using the longest string
+    function normalizeDates(dates) {
+        var out = {
+            alt: [],
+            date: new Date(NaN),
+            str: 'UNKNOWN'
+        };
+        var n = 0;
+        dates.forEach(function (d) {
+            out.alt.push(d);
+            if (n < d.length) {
+                n = d.length;
+                out.date = normalizeDate(d);
+                out.str = d;
+                if (!d.valueOf()) {
+                    n = 0;
+                }
+            }
+        });
+        return out.date;
+    }
+
+    function normalizeDatePlace(source) {
+        var target = {
+            date: null,
+            place: null
+        };
+
+        (source || []).forEach(function (b) {
+            target.date = normalizeDates(b.DATE || []);
+            normalizePlaces(
+                b.PLAC || [],
+                function (p) {
+                    target.place = p;
+                }
+            );
+        });
+        return target;
+    }
+
+    function normalizeBirth(person) {
+        person.born = normalizeDatePlace(person.BIRT);
+        delete person.BIRT;
+    }
+
+    function normalizeRes(person) {
+        person.res = (person.RESI || []).map(function (r) {
+            return normalizeDatePlace([r]);
+        });
+        delete person.RESI;
+    }
+
+    function normalizeDeath(person) {
+        person.died = normalizeDatePlace(person.DEAT);
+        delete person.DEAT;
+    }
+
+    function normalizeBuried(person) {
+        person.buried = normalizeDatePlace(person.BURI);
+        delete person.BURI;
+    }
+
+    function normalizePlace(place, done) {
+        // hmmm don't realy want an async method here...
+        // maybe preload all PLAC tags before normalization
+        gedcom.georeference(place, done);
+    }
+    
+    function normalizePlaces(places, done) {
+        if (places && places[0]) {
+            gedcom.georeference(places[0], done);
+        } else {
+            done(null);
+        }
+    }
+
+    function normalizeSex(person) {
+        var s;
+        if (person.SEX &&
+            person.SEX[0] &&
+            typeof person.SEX[0].value === 'string'
+           ) {
+            s = person.SEX[0].value;
+            if (s.toUpperCase() === 'M') {
+                person.sex = 'M';
+            } else if (s.toUpperCase() === 'F') {
+                person.sex = 'F';
+            } else {
+                person.sex = 'O'; // other, I guess
+            }
+        } else {
+            person.sex = null;
+        }
+        delete person.SEX;
+    }
+
+    function normalizePerson(person) {
+        delete person.CHAN;
+        delete person._UID;
+        delete person.type;
+        normalizeSex(person);
+        normalizeName(person);
+        normalizeBirth(person);
+        normalizeRes(person);
+        normalizeDeath(person);
+        normalizeBuried(person);
+    }
+
+    function normalizeFamily(family) {
+    }
+
+    /**
+     * Normalize important fields in the record list and remove duplicate
+     * keys to simplify database querying.
+     *
+     * @todo Maybe provide custom handlers to override defaults
+     *
+     * @param {object[]} records Output from gedcom.parse
+     * @returns {object}
+     */
+     gedcom.normalize = function (records) {
+         var out = {
+             people: [],
+             families: [],
+             ignored: []
+         };
+         records.forEach(function (record) {
+            if (typeof record.type !== 'string') {
+                out.ignored.push(record);
+            } else if (record.type.toUpperCase() === 'INDI') {
+                out.people.push(record);
+                normalizePerson(record);
+            } else if (record.type.toUpperCase() === 'FAM') {
+                out.families.push(record);
+                normalizeFamily(record);
+            }
+         });
+         return out;
+     };
 
     GLOBAL.gedcom = gedcom;
 })(window);
